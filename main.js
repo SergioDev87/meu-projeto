e88959cf53efd53f9af8ac8ca53e506d0013c444
sergiosousahome@gmail.com <sergiosousahome@gmail.com>
2023-06-27 13:18:31 -0300

    "add titulo umbrella"

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-console.log('Hello World!');
-
 document.getElementById('btn').addEventListener('click', () => chat());
 
 
+document.getElementById('btn-menu').addEventListener('click', () => abrirMenu());
 function chat() {
-  nome = prompt("Qual seu nome?");
-  alert('Olá ' + nome);
-  deMaior = confirm(nome + " você é de maior?");
-  if (deMaior){
-    alert(nome + ' você declarou ser de maior!')
+  alert('oi');
+}
+
+let barra = document.getElementById('barra')
+let tema = 'claro'
+
+function mudarTema(){
+  if(tema === 'claro'){
+  barra.style.backgroundColor = '#222'
+  tema = 'escuro'
+} else {
+  barra.style.backgroundColor = '#fff'
+  tema = 'claro'
+}
+  
+}
+
+let menu = document.getElementById('menu')
+let menuStatus= 'off'
+
+
+function abrirMenu() {
+  if (menuStatus === 'off') {
+    menu.style.marginLeft = 0;
+    menuStatus= 'on'
   } else {
-    alert(nome + ' você declarou ser de menor!');
+    menu.style.marginLeft = '-250px';
+    menuStatus='off'
   }
 }
+
+document.getElementById('btn-tema').addEventListener('click', () => mudarTema(tema));
+
+
