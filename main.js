8a4992876299d4aaf4139020d5d674dacb192786
sergiosousahome@gmail.com <sergiosousahome@gmail.com>
2023-06-29 13:59:46 -0300

    "add tema escuro"

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.getElementById('btn').addEventListener('click', () => chat());
+
 
 
 document.getElementById('btn-menu').addEventListener('click', () => abrirMenu());
@@ -6,15 +6,22 @@ function chat() {
   alert('oi');
 }
 
+let body = document.getElementById('body')
 let barra = document.getElementById('barra')
 let tema = 'claro'
 
 function mudarTema(){
   if(tema === 'claro'){
   barra.style.backgroundColor = '#222'
+  barra.style.color = '#fff'
+  body.style.backgroundColor = 'black'
+  body.style.color = 'gray'
   tema = 'escuro'
 } else {
   barra.style.backgroundColor = '#fff'
+  barra.style.color = 'black'
+  body.style.backgroundColor = '#fff'
+  body.style.color = 'black'
   tema = 'claro'
 }
   
@@ -36,4 +43,25 @@ function abrirMenu() {
 
 document.getElementById('btn-tema').addEventListener('click', () => mudarTema(tema));
 
+// slide
+let slide1 = 'assets/img/slide-1.jpg'
+let slide2 = 'assets/img/slide-2.jpg'
+let slide3 = 'assets/img/slide-3.jpg'
+let slider = document.getElementById('slide')
+let i = 1;
+
+
+let intevalo = window.setInterval(function mudarSlide(){
+  if(i===1) {
+    slider.src = slide2
+    i = 2
+  } else if(i===2){
+    slider.src = slide3
+    i = 3
+  } else {
+    slider.src = slide1
+    i = 1
+  }
+  
+}, 3000)
 
